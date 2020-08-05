f1407a334e311b2e3cf98df2a5c62bdeb3df9072
skullmind <xujianhuaap@gmail.com>
2020-08-05 21:32:50 +0800

    feature[RightBar.js]:fold group

--- src/main/RightBar.js
+++ src/main/RightBar.js
@@ -6,8 +6,52 @@ import {
   TouchableOpacity,
   StyleSheet,
 } from 'react-native';
-
-class ItemView1 extends Component {
+const DATA = [
+  {
+    title: '核心组件',
+    show: true,
+    data: [
+      {title: '核心组件和api'},
+      {title: 'ActivityIndicator'},
+      {title: 'Button'},
+      {title: 'FlatList'},
+      {title: 'Image'},
+      {title: 'ImageBackground'},
+      {title: 'KeyboardAvoidingView'},
+      {title: 'Modal'},
+      {title: 'Pressable'},
+      {title: 'RefreshControl'},
+      {title: 'SafeAreaView'},
+      {title: 'ScrollView'},
+      {title: 'SectionList'},
+      {title: 'StatusBar'},
+      {title: 'Switch'},
+      {title: 'Text'},
+      {title: 'TextInput'},
+      {title: 'TouchableHighLight'},
+      {title: 'TouchableOpacity'},
+      {title: 'TouchableWithoutFeedback'},
+      {title: 'View'},
+      {title: 'VirtualizedList'},
+      {title: 'Android组件'},
+      {title: 'DrawerLayoutAndroid'},
+      {title: 'TouchableNativeFeedback'},
+      {title: 'IOS组件'},
+      {title: 'InputAccessoryView'},
+    ],
+  },
+  {
+    title: '属性',
+    show: true,
+    data: [
+      {title: 'Image样式属性'},
+      {title: '布局属性'},
+      {title: '阴影样式属性'},
+      {title: 'View样式属性'},
+    ],
+  },
+];
+class ItemView extends Component {
   render() {
     return (
       <TouchableOpacity onPress={() => null}>
@@ -21,61 +65,53 @@ class ItemView1 extends Component {
   }
 }
 class RightBar extends Component {
-  DATA = [
-    {
-      title: '核心组件',
-      data: [
-        {title: '核心组件和api'},
-        {title: 'ActivityIndicator'},
-        {title: 'Button'},
-        {title: 'FlatList'},
-        {title: 'Image'},
-        {title: 'ImageBackground'},
-        {title: 'KeyboardAvoidingView'},
-        {title: 'Modal'},
-        {title: 'Pressable'},
-        {title: 'RefreshControl'},
-        {title: 'SafeAreaView'},
-        {title: 'ScrollView'},
-        {title: 'SectionList'},
-        {title: 'StatusBar'},
-        {title: 'Switch'},
-        {title: 'Text'},
-        {title: 'TextInput'},
-        {title: 'TouchableHighLight'},
-        {title: 'TouchableOpacity'},
-        {title: 'TouchableWithoutFeedback'},
-        {title: 'View'},
-        {title: 'VirtualizedList'},
-        {title: 'Android组件'},
-        {title: 'DrawerLayoutAndroid'},
-        {title: 'TouchableNativeFeedback'},
-        {title: 'IOS组件'},
-        {title: 'InputAccessoryView'},
-      ],
-    },
-    {
-      title: '属性',
-      data: [
-        {title: 'Image样式属性'},
-        {title: '布局属性'},
-        {title: '阴影样式属性'},
-        {title: 'View样式属性'},
-      ],
-    },
-  ];
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      data: [],
+    };
+  }
+
+  handleSectionHeader = ({section}) => {
+    const currentData = this.state.data;
+    currentData.map(({item, index}) => {
+      if (item === section) {
+        if (section.show) {
+          item.data = [];
+        } else {
+          item.data = DATA[index];
+        }
+        item.show = !item.show;
+      }
+    });
+    this.setState({
+      data: currentData,
+    });
+  };
+
+  componentDidMount() {
+    this.setState({
+      data: DATA,
+    });
+  }
+
   render() {
     return (
       <View>
         <SectionList
-          sections={this.DATA}
+          sections={this.state.data}
           keyExtractor={(item, index) => item + index}
           renderItem={({item, index}) => (
-            <ItemView1 title={item.title} index={index} />
+            <ItemView title={item.title} index={index} />
           )}
-          renderSectionHeader={({section: {title}}) => (
-            <Text style={styles.sectionHeader}>{title}</Text>
+          renderSectionHeader={({section}) => (
+            <Text
+              style={styles.sectionHeader}
+              onPress={() => this.handleSectionHeader({section})}>
+              {section.title}
+            </Text>
           )}
+          extraData={this.state}
         />
       </View>
     );
