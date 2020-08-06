d47f003d389bd479dde9d2eaa9a0b6da5c8974c8
skullmind <xujianhuaap@gmail.com>
2020-08-06 16:32:27 +0800

    feature[RightBar.js]:array deep clone by json

--- src/main/RightBar.js
+++ src/main/RightBar.js
@@ -6,51 +6,8 @@ import {
   TouchableOpacity,
   StyleSheet,
 } from 'react-native';
-const DATA = [
-  {
-    title: '核心组件',
-    show: true,
-    data: [
-      {title: '核心组件和api'},
-      {title: 'ActivityIndicator'},
-      {title: 'Button'},
-      {title: 'FlatList'},
-      {title: 'Image'},
-      {title: 'ImageBackground'},
-      {title: 'KeyboardAvoidingView'},
-      {title: 'Modal'},
-      {title: 'Pressable'},
-      {title: 'RefreshControl'},
-      {title: 'SafeAreaView'},
-      {title: 'ScrollView'},
-      {title: 'SectionList'},
-      {title: 'StatusBar'},
-      {title: 'Switch'},
-      {title: 'Text'},
-      {title: 'TextInput'},
-      {title: 'TouchableHighLight'},
-      {title: 'TouchableOpacity'},
-      {title: 'TouchableWithoutFeedback'},
-      {title: 'View'},
-      {title: 'VirtualizedList'},
-      {title: 'Android组件'},
-      {title: 'DrawerLayoutAndroid'},
-      {title: 'TouchableNativeFeedback'},
-      {title: 'IOS组件'},
-      {title: 'InputAccessoryView'},
-    ],
-  },
-  {
-    title: '属性',
-    show: true,
-    data: [
-      {title: 'Image样式属性'},
-      {title: '布局属性'},
-      {title: '阴影样式属性'},
-      {title: 'View样式属性'},
-    ],
-  },
-];
+import DATA from '../json/RightBar.json';
+const DATA1 = JSON.parse(JSON.stringify(DATA));
 class ItemView extends Component {
   render() {
     return (
@@ -72,28 +29,34 @@ class RightBar extends Component {
     };
   }
 
-  handleSectionHeader = ({section}) => {
-    const currentData = this.state.data;
-    currentData.map(({item, index}) => {
-      if (item.title === section.title) {
-        if (section.show) {
-          item.data = [];
-        } else {
-          item.data = DATA[index];
-        }
-        item.show = !item.show;
-      }
+  componentDidMount() {
+    let dataCopy = [];
+    Object.assign(dataCopy, DATA);
+    this.setState({
+      data: dataCopy,
     });
+  }
+
+  handleSectionHeader = ({section}) => {
+    let currentData = this.state.data;
+    for (let index = 0; index < currentData.length; index++) {
+      const item = currentData[index];
+      if (item.title !== section.title) {
+        continue;
+      }
+      if (section.show) {
+        item.data = [];
+        item.show = false;
+      } else {
+        item.data = DATA1[index].data;
+        item.show = true;
+      }
+      break;
+    }
     this.setState({
       data: currentData,
     });
   };
-
-  componentDidMount() {
-    this.setState({
-      data: DATA,
-    });
-  }
 
   render() {
     return (
@@ -111,7 +74,6 @@ class RightBar extends Component {
               {section.title}
             </Text>
           )}
-          extraData={this.state}
         />
       </View>
     );
